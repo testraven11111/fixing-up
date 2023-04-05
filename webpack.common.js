eca87e2e6f5dc261d31288c7f7f3cfdfd59aa32e
testraven11111 <87571952+testraven11111@users.noreply.github.com>
2023-04-05 23:48:39 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+    entry: {
+        index: './src/index.js',
     },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
@@ -21,48 +38,6 @@ module.exports = {
                     },
                 },
             },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][ext]'
-                }
-            },
-            {
-                test: /\.(jpe?g)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][ext]'
-                },
-                use: [
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 80
-                            }
-                        }
-                    }
-                ]
-            }
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'src/assets',
-                    to: 'assets',
-                },
-            ],
-        }),
-    ],
 };
